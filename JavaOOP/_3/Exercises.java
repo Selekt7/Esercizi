211960df8fe25e1cdad90f08e48bd60232f64661
Gabriele <gabrieleingrassia3@gmail.com>
2023-03-21 10:58:58 +0100

    ex 3 completato

---
+++ JavaOOP/_3/Exercises.java
@@ -0,0 +1,63 @@
+package JavaOOP._3;
+
+import JavaOOP._3.Student;
+
+public class Exercises {
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+    }
+
+    /**
+     * 1: Copy over the student class
+     *
+     *    Make sure there are 5 fields inside, if they are not simply create some more.
+     *    make every field private first.
+     *
+     *    Then create getters and setters for each of these 5 fields.
+     *
+     *    Once ready use all 5 below
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+
+        // Write and use your 5 getters and setters!
+        Student student = new Student();
+        student.setName("Mario");
+        student.setAge(25);
+        student.setIsOffSite(true);
+        student.setVoteInDecimal(10);
+        student.setFavoriteSubject("Math");
+        System.out.println(student.getName());
+        System.out.println(student.getAge());
+        System.out.println(student.isOffSite());
+        System.out.println(student.getVoteInDecimal());
+        System.out.println(student.getFavoriteSubject());
+    }
+
+    /**
+     * 2: Create 3 setter constraints for the student class
+     *
+     *    Think about what reasonably constraints are?
+     *
+     *    Should a student be able to be a negative age?
+     *
+     *    Create a counter for one of the getters, try to think about
+     *    what other code could be inside a getter? Why not write it?
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        Student student = new Student();
+        student.setVoteInDecimal(10);
+        student.setVoteInDecimal(7);
+        student.setVoteInDecimal(2);
+        student.getVoteRegister();
+        student.setIsOffSite(true);
+        student.setIsOffSite(false);
+        student.setIsOffSite(true);
+        int counter= student.getCounterChangeSite();
+        System.out.println(counter);
+        //Write your code here
+    }
+
+}
---
+++ JavaOOP/_3/Student.java
@@ -0,0 +1,80 @@
+package JavaOOP._3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String name;
+    private int age;
+    private boolean isOffSite;
+    private String favoriteSubject;
+    private int voteInDecimal;
+    private static int minAge = 18;
+    private static int minVote = 0;
+    private static int maxVote = 10;
+    private List<Integer> voteRegister=new ArrayList<>();
+
+    private int counterChangeSite=0;
+
+    public int getCounterChangeSite() {
+        return counterChangeSite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (minAge <= age) {
+            this.age = age;
+        } else {
+            System.out.println("You must be of legal age to take the course");
+        }
+    }
+
+    public boolean isOffSite() {
+        return isOffSite;
+    }
+
+    public void setIsOffSite(boolean offSite) {
+        counterChangeSite++;
+        isOffSite = offSite;
+    }
+
+    public String getFavoriteSubject() {
+        return favoriteSubject;
+    }
+
+    public void setFavoriteSubject(String favoriteSubject) {
+        this.favoriteSubject = favoriteSubject;
+    }
+
+    public int getVoteInDecimal() {
+        return voteInDecimal;
+    }
+
+    public void setVoteInDecimal(int voteInDecimal) {
+        if (minVote <= voteInDecimal && maxVote >= voteInDecimal) {
+            this.voteInDecimal = voteInDecimal;
+            voteRegister.add(voteInDecimal);
+        } else {
+            System.out.println("The vote must be in tenths");
+        }
+    }
+    public  void getVoteRegister(){
+        for (int i=0; i<voteRegister.size(); i++) {
+            System.out.println(voteRegister.get(i));
+        }
+    }
+
+
+}
+
